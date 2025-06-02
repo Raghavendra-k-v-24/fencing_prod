d128517af9a9f2d852afdb9de380567e1c4ec27f
Raghavendra-k-v-24 <raghavendrakv24@gmail.com>
2025-06-02 22:06:40 +0530

    added search functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from "react";
 import Logo from "../assets/Logo.avif";
 import { Label } from "@/components/ui/label";
@@ -12,6 +14,23 @@ import {
 import { Button } from "@/components/ui/button";
 import axios from "axios";
 import { toast } from "sonner";
+
+import { Check } from "lucide-react";
+import { cn } from "@/lib/utils";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { ChevronDown } from "lucide-react";
 
 const Login = ({ students, getStudents, group, setGroup }) => {
   const groups = [
@@ -60,6 +79,32 @@ const Login = ({ students, getStudents, group, setGroup }) => {
       );
     }
   };
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+  const frameworks = [
+    {
+      value: "next.js",
+      label: "Next.js",
+    },
+    {
+      value: "sveltekit",
+      label: "SvelteKit",
+    },
+    {
+      value: "nuxt.js",
+      label: "Nuxt.js",
+    },
+    {
+      value: "remix",
+      label: "Remix",
+    },
+    {
+      value: "astro",
+      label: "Astro",
+    },
+  ];
+
+  console.log(selectedStudentId);
 
   return (
     <div className="size-full flex flex-col justify-center items-center gap-5">
@@ -92,27 +137,53 @@ const Login = ({ students, getStudents, group, setGroup }) => {
           <Label htmlFor="studentSelector" className="text-2xl">
             Students
           </Label>
-          <Select
-            id="studentSelector"
-            onValueChange={(id) => setSelectedStudentId(id)}
-          >
-            <SelectTrigger className="w-[180px] h-[40px]">
-              <SelectValue placeholder="Select a Student" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                {students.map((item) => (
-                  <SelectItem
-                    value={item.id}
-                    key={item.id}
-                    className={item.status == "In" && "bg-green-50"}
-                  >
-                    {item.name}
-                  </SelectItem>
-                ))}
-              </SelectGroup>
-            </SelectContent>
-          </Select>
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <Button
+                variant="outline"
+                role="combobox"
+                aria-expanded={open}
+                className="w-[180px] h-[40px] justify-between font-normal"
+              >
+                {selectedStudentId
+                  ? students.find((student) => student.id === selectedStudentId)
+                      .name
+                  : "Select a Student"}
+                <ChevronDown className="opacity-50" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+              <Command>
+                <CommandInput placeholder="Search student" className="h-9" />
+                <CommandList>
+                  <CommandEmpty>No framework found.</CommandEmpty>
+                  <CommandGroup>
+                    {students.map((student) => (
+                      <CommandItem
+                        key={student.id}
+                        value={student.id}
+                        onSelect={() => {
+                          setSelectedStudentId(student.id);
+                          setOpen(false);
+                        }}
+                        className={student.status == "In" && "bg-green-50"}
+                      >
+                        {student.name}
+                        <Check
+                          className={cn(
+                            "ml-auto",
+                            value === student.value
+                              ? "opacity-100"
+                              : "opacity-0"
+                          )}
+                        />
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
         </div>
       </div>
       <div className="flex gap-5">
